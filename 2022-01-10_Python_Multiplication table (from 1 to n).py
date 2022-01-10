619e9db82257091d28893d22a4e25cb7bd2a9f36
erniejie <46618858+Erniejie@users.noreply.github.com>
2022-01-10 16:56:39 +0000

    Add files via upload

---
+++ 2022-01-10_Python_Multiplication table (from 1 to n).py
@@ -0,0 +1,14 @@
+#Multiplication table (from 1 to n) in Python
+#Computer Programming Tutor,Jan10,2022
+
+# To Take Input from the user:
+
+NO= int(input("Enter The Number Multiplication Table of:")
+        )
+# Iterate 18 times from i = 1 to 18
+
+for i in range(1,19):
+    print(NO,"x",i,"=",NO*i
+          )
+
+
